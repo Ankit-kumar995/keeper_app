df22f46bc13113262f7b5044adffd29ceefba7a8
Ankit-kumar995 <Ankitkr212023@gmail.com>
2026-05-28 20:39:57 +0530

    connect frontend with deployed backend

--- Frontend/keeper-app/src/api/axios.js
+++ Frontend/keeper-app/src/api/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: '/api', // ← sirf yeh change karo
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
